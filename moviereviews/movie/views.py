2eca0dc53f8e3d4fa90142c0a2ea45969bd6c215
Gerry Linux Laptop <gerardmijares39@gmail.com>
2022-08-29 10:48:53 +0200

    Added Movie model

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -4,7 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {'name':'Greg Lim'})
+    searchTerm = request.GET.get('searchMovie')
+    return render(request, 'home.html', {'searchTerm':searchTerm})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
+
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email':email})
--- moviereviews/moviereviews/urls.py
+++ moviereviews/moviereviews/urls.py
@@ -15,10 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from movie import views as movieViews
+from django.conf import settings
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', movieViews.home),
-    path('about/', movieViews.about),        
+    path('about/', movieViews.about),  
+    path('signup/', movieViews.signup, name='signup'),  
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
